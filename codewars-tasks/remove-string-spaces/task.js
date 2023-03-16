b6a6026aee7c123b0bd502de3789700bded32574
salkyn <omursalkyn@gmail.com>
2023-03-16 13:35:09 +0600

    -solved noStringSpace task

---
+++ codewars-tasks/remove-string-spaces/task.js
@@ -0,0 +1,13 @@
+function noSpace(x){
+let str='';
+ for(let i =0;i<x.length;i++){
+    if(x[i]!=' '){
+        str+=x[i];
+    }
+ }
+ return str;
+}
+
+let str ='hello world';  
+
+console.log(noSpace(str));
