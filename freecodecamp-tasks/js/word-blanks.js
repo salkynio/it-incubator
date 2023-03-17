b6b39e95dda5051cf51823d555e6d9507019bffb
salkyn <omursalkyn@gmail.com>
2023-03-17 12:19:04 +0600

    -solved word-blanks method

---
+++ freecodecamp-tasks/js/word-blanks.js
@@ -0,0 +1,9 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My" +" " + myNoun + " " + "is" + " " + myAdjective + "," + "he" +" " + "can" + " " + myVerb + " " + myAdverb +"."; // Change this line
+console.log(wordBlanks);
+// Only change code above this line
